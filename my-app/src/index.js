a24b341319e49d38570415c9bddefb229354e123
Vibhas <vkam0002@student.monash.edu>
2019-12-29 02:44:00 +1100

    Created component for weight

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,54 @@ import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
+//  BMI = (Weight/2.205) / (Height/39.37)2
+
+class WeightComponent extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            weight: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({weight: event.target.value});
+    }
 
 
+    render(){
+        return (
+            <form>
+                <label>
+                    Enter your weight (in kg)
+                    <input type="text" name="name" value={this.state.weight} onChange={this.handleChange} />
+                </label>
+                {/* <input type="submit" value="Submit" /> */}
+                {this.state.weight}
+            </form>
+            
+        );
+    }
+}
+
+
+class Interface extends React.Component{
+    render(){
+        return (
+            // "This is the main UI"
+            <div>
+                <WeightComponent />
+            </div>
+        );
+    }
+}
 
 
 ReactDOM.render(
-    'hello', 
+    <Interface />, 
     document.getElementById('root')
 );
 
